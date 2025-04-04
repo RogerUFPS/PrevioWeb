28dadd57d5c8e93731903cf40640e8691b2e7367
RogerUFPS <166035216+RogerUFPS@users.noreply.github.com>
2025-04-04 11:55:00 -0500

    Parte 2

---
+++ js/api.js
@@ -0,0 +1,107 @@
+// API Configuration
+const API_URL = 'https://dvkvmjdefaytycdbsntd.supabase.co/rest/v1';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2a3ZtamRlZmF5dHljZGJzbnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MjE1MjAsImV4cCI6MjA1OTI5NzUyMH0.wYHbfTAJyIp2CLfU4LcIJfJAMrVq41zUK6kw5GZ01ts';
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+// API Service
+const api = {
+  // Headers for API requests
+  headers: {
+    'apikey': API_KEY,
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Prefer': 'return=representation',
+    'Authorization': `Bearer ${API_KEY}`,
+  },
+
+  // Fetch all students
+  async getAsignatures() {
+    try {
+      const response = await fetch(`${API_URL}/asignatura`, {
+        method: 'GET',
+        headers: api.headers
+      });
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch students');
+      }
+      
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      throw error;
+    }
+  },
+
+  async addAsignature(code, name, description) {
+    try {
+
+      const response = await fetch(`${API_URL}/asignatura`, {
+        method: 'POST',
+        headers: api.headers,
+        body: JSON.stringify({
+          code: code,
+          name: name,
+          description: description,
+        }),
+      });
+
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+  
+  async updateAsign(befCode, name, description) {
+    
+    try {
+      const response = await fetch(`${API_URL}/asignatura?codigo=eq.${befCode}`, {
+        method: 'PATCH',
+        headers: api.headers,
+        body: JSON.stringify({
+          name: name,
+          description: description,
+        }),
+      });
+      return response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+
+  async getAsignStudents(code) {
+    try {
+      const response = await fetch(`${API_URL}/matricula?codigo_asignatura=eq.${code}`, {
+        method: 'GET',
+        headers: api.headers
+      });
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch students');
+      }
+      
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      throw error;
+    }
+  },
+
+  async getStudents(code) {
+    try {
+      const response = await fetch(`${API_URL}/alumno?codigo=eq.${code}`, {
+        method: 'GET',
+        headers: api.headers
+      });
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch students');
+      }
+      
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      throw error;
+    }
+  },
+}
---
+++ js/app.js
@@ -0,0 +1,134 @@
+
+let asignatures;
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Sample student data
+
+
+    const asignList = document.getElementById('asignCards');
+    const template = document.getElementById('templateAsignCards');
+
+    // Render students
+    async function renderAsignatures() {
+        asignatures = await api.getAsignatures();
+        asignList.innerHTML = '';
+        let i = 0;
+        asignatures.forEach(asign => {
+            //console.log(asign);
+
+            const clone = template.content.cloneNode(true);
+
+            clone.querySelector('.name').textContent = asign.nombre;
+            clone.querySelector('.id').textContent = `ID: ${asign.codigo}`;
+            clone.querySelector('.creditos').textContent = asign.creditos;
+            clone.querySelector('.description').textContent = asign.descripcion;
+
+            clone.firstElementChild.classList.add(`${i}`);
+
+            asignList.appendChild(clone);
+            i++;
+        });
+    }
+
+    renderAsignatures();
+});
+
+
+async function loadTechnologies(code) {
+    const techList = document.getElementById("techList");
+    const template = document.getElementById("techTemplate");
+
+    techList.innerHTML='';
+    const n = await api.getTechnology(code);
+
+    let x = ["Java", "Python",  "PhP", "React", "Angular"];
+    let z = ["./img/technology/java.png", 
+        "./img/technology/python.png", 
+        "./img/technology/php.png", 
+        "./img/technology/react.png", 
+        "./img/technology/angular.png"];
+
+    n.forEach(tech => {
+
+        const clone = template.content.cloneNode(true);
+
+        clone.querySelector('.tech-icon').src = z[tech.technology_code-1];
+        clone.querySelector('.tech-name').textContent = x[tech.technology_code-1]; 
+
+        for(let i = 0;i<tech.level;i++){
+            let f = document.createElement("img");
+            f.src = "./img/star.png"
+            f.classList.add("tech-icon");
+            clone.querySelector(".tech-rate").appendChild(f);
+        }
+        
+        techList.appendChild(clone);
+    });
+}
+
+//Add student
+
+function addAsign() {
+    let e = document.getElementsByName("nAsign");
+    api.addAsignature(e[0].value, e[1].value, e[2].value);
+    
+    // setTimeout(() => {
+    //     location.reload();
+    // }, 500);
+}
+
+let actIndex;
+
+function getAsign(e) {
+    actIndex = e.parentNode.parentNode.parentNode.classList[1];
+
+    let inputs = document.getElementsByName("eAsign");
+
+    inputs[0].value = asignatures[actIndex].codigo;
+    inputs[1].value = asignatures[actIndex].creditos;
+    inputs[2].value = asignatures[actIndex].nombre;    
+    inputs[3].value = asignatures[actIndex].descripcion;
+}
+
+function update() {
+
+    let inputs = document.getElementsByName("eAsign");
+    api.updateAsign(asignatures[actIndex].codigo, inputs[1].value, inputs[2].value);
+    
+    // setTimeout(() => {
+    //     location.reload();
+    // }, 500);
+
+}
+
+
+let totStd = 0;
+
+async function putStudents(e) {
+
+    const studentList = document.getElementById('studentList');
+    const template = document.getElementById('templateStudentCards');
+
+    actIndex = e.parentNode.parentNode.parentNode.classList[1];
+
+    let asStudents = await api.getAsignStudents(asignatures[actIndex].codigo);
+
+    for (const code of asStudents) { 
+        let act = await api.getStudents(code.codigo_alumno); 
+
+        console.log(act);
+
+        for(const a of act){
+            const clone = template.content.cloneNode(true);
+            clone.querySelector('.nameE').textContent = a.nombre;
+            clone.querySelector('.idE').textContent = `ID: ${a.codigo}`;
+            studentList.appendChild(clone);
+            totStd++;
+        }
+    }
+
+    document.getElementById('vAsign').textContent = asignatures[actIndex].nombre;
+    document.getElementById('vCredits').textContent = asignatures[actIndex].creditos;
+    document.getElementById('vDescr').textContent = asignatures[actIndex].descripcion;
+    document.getElementById('totalEst').textContent = totStd + "/" + 3;
+}
